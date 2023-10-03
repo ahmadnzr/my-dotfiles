c2e972eaf5d72597e17ecc51fa9bf1c62ba15c94
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-10-03 22:18:30 +0000

    feat: telescope setup

--- .config/nvim/lua/nizardev/core/keymaps.lua
+++ .config/nvim/lua/nizardev/core/keymaps.lua
@@ -24,12 +24,3 @@ keymap.set("n", "to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "tq", ":tabclose<CR>") -- close current tab
 keymap.set("n", "tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "tp", ":tabp<CR>") -- go to previous tab
-
--- plugins keymaps
-
--- telescope
-keymap.set("n", "ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
