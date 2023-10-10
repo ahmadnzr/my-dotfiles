15063a4a3790719651b11ee0f93ad5588170d02d
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-10-10 22:50:51 +0700

    fix: colorscheme

--- .config/nvim/lua/nizardev/lazy.lua
+++ .config/nvim/lua/nizardev/lazy.lua
@@ -13,7 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({ { import = "nizardev.plugins" }, { import = "nizardev.plugins.lsp" } }, {
   install = {
-    colorscheme = { "nightfly" },
+    colorscheme = { "tokyonight" },
   },
   checker = {
     enabled = true,
