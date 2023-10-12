acd927908ca76ec3e275941ca166575b0364ad94
Ahmad Nizar <ahmadnzr.dev@gmail.com>
2023-10-12 14:56:39 +0700

    feat: add keymap to open terminal

--- .config/nvim/lua/nizardev/core/keymaps.lua
+++ .config/nvim/lua/nizardev/core/keymaps.lua
@@ -19,8 +19,11 @@ keymap.set("n", "sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "se", "<C-w>=") -- make split windows equal width
 keymap.set("n", "sq", ":close<CR>") -- close current window
 
-
 keymap.set("n", "to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "tq", ":tabclose<CR>") -- close current tab
 keymap.set("n", "tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "tp", ":tabp<CR>") -- go to previous tab
+
+keymap.set("n", "<leader>tn", ":tabnew +term<CR>") -- open terminal in new tab
+keymap.set("n", "<leader>ts", ":vsplit +term<CR>") -- open terminal in vertical split
+keymap.set("n", "<leader>th", ":split +term<CR>") -- open terminal in horizontal split
